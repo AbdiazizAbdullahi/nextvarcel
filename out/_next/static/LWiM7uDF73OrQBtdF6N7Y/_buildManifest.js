96ffcaf521b30d885b448a5f1efdffc11fc7d9b7
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2024-08-02 11:47:11 +0300

    next

---
+++ out/_next/static/LWiM7uDF73OrQBtdF6N7Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/628765f20b848f76.css","static/chunks/pages/index-aae63f14760948b6.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
